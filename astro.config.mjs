80adba6044795aa970243dbbac17d1b83dd1b160
guerinoj <guerino.jonathan@gmail.com>
2023-10-11 14:51:42 +0200

    Config for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(), react()]
+  integrations: [tailwind(), compress(), react()],
+  site: 'https://www.fluid-crm.fr',
 });
